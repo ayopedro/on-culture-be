d48af9945a55ddc794a25e83e2177838971141e6
Ayotunde <ayopedro2012@gmail.com>
2024-05-27 19:06:47 +0100

    add modules for product, customer and prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { CustomersModule } from './customers/customers.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [OrdersModule, PrismaModule],
+  imports: [OrdersModule, PrismaModule, CustomersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
