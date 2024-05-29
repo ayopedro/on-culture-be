fada45cf9312dd7f6fdba402582ace54e1f148ea
Ayotunde <ayopedro2012@gmail.com>
2024-05-29 19:45:57 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -60,6 +60,10 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
+  app.enableCors({
+    origin: '*',
+  });
+
   await app.listen(port);
 }
 
